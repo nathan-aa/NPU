403dd89c6ee3b44f094a573ccf0037be07501427
Nathan <nathanayalon@gmail.com>
2025-02-24 22:14:59 +0100

    Fix UserManger header file

--- src/user_api/user_manager.h
+++ src/user_api/user_manager.h
@@ -1,25 +1,15 @@
-#include "user_manager.h"
-#include <bcrypt/BCrypt.hpp>
+#ifndef USER_MANAGER_H
+#define USER_MANAGER_H
+#include "user_dao.h"
 
-crow::json::wvalue UserManager::create_user(const std::string& username, const std::string& email, const std::string& password) {
-    std::string hashed_password = BCrypt::generateHash(password);
-    return userDAO.create_user(username, email, hashed_password);
-}
+class UserManager {
+public:
+    crow::json::wvalue create_user(const std::string& username, const std::string& email, const std::string& password);
+    crow::json::wvalue get_user(int id);
+    crow::json::wvalue update_user(int id, const std::string& username, const std::string& email);
+    crow::json::wvalue delete_user(int id);
 
-crow::json::wvalue UserManager::get_user(int id) {
-    return userDAO.get_user(id);
-}
-
-
-crow::json::wvalue UserManager::update_user(int id, const std::string& username, const std::string& email) {
-    return userDAO.update_user(id, username, email);
-}
-
-crow::json::wvalue UserManager::delete_user(int id) {
-    return userDAO.delete_user(id);
-}
-
-
-crow::json::wvalue UserManager::authenticate_user(const std::string& email, const std::string& password) {
-    return userDAO.authenticate_user(email, password);
-}
+    crow::json::wvalue authenticate_user(const std::string& email, const std::string& password);
+private:
+    UserDAO userDAO;
+};
